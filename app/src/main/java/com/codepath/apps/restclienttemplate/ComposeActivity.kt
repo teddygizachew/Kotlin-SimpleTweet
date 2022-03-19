25283b756fc792a56cd594bd3fdf229220d507ad
teddygizachew <teddygizachew1@gmail.com>
2022-03-18 23:57:31 -0400

    SimpleTweet Part 2

---
+++ app/src/main/java/com/codepath/apps/restclienttemplate/ComposeActivity.kt
@@ -0,0 +1,80 @@
+package com.codepath.apps.restclienttemplate
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.text.Editable
+import android.text.TextWatcher
+import android.util.Log
+import android.widget.Button
+import android.widget.EditText
+import android.widget.TextView
+import android.widget.Toast
+import androidx.appcompat.app.ActionBar
+import com.codepath.apps.restclienttemplate.models.Tweet
+import com.codepath.asynchttpclient.callback.JsonHttpResponseHandler
+import okhttp3.Headers
+
+class ComposeActivity : AppCompatActivity() {
+
+    lateinit var etCompose: EditText
+    lateinit var btnTweet: Button
+
+    lateinit var client: TwitterClient
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_compose)
+
+        // in Activity#onCreate
+        supportActionBar!!.displayOptions = ActionBar.DISPLAY_SHOW_CUSTOM
+        supportActionBar!!.setCustomView(R.layout.actionbar_title)
+
+        etCompose = findViewById(R.id.etTweetCompose)
+        btnTweet = findViewById(R.id.btnTweet)
+
+        client = TwitterApplication.getRestClient(this)
+
+        btnTweet.setOnClickListener {
+            // Grab the content of the edittext (etCompose)
+            val tweetContent = etCompose.text.toString()
+
+            // 1. Make sure the tweet isn't empty
+            if (tweetContent.isEmpty()) {
+                Toast.makeText(this, "Empty tweets are not allowed!", Toast.LENGTH_SHORT).show()
+                // TODO: Look into showing a SnackBar message
+            }
+            // 2. Make sure the tweet is under character count
+            if (tweetContent.length > 140) {
+                Toast.makeText(this, "Tweet is too long: Limit is 140 characters!", Toast.LENGTH_SHORT).show()
+            } else {
+                client.publishTweet(tweetContent, object : JsonHttpResponseHandler() {
+                    override fun onSuccess(statusCode: Int, headers: Headers?, json: JSON) {
+                        Log.i(TAG, "Successfully published tweet!")
+
+                        val tweet = Tweet.fromJson(json.jsonObject)
+
+                        val intent = Intent()
+                        intent.putExtra("tweet", tweet)
+                        setResult(RESULT_OK, intent)
+                        finish()
+                    }
+
+                    override fun onFailure(
+                        statusCode: Int,
+                        headers: Headers?,
+                        response: String?,
+                        throwable: Throwable?
+                    ) {
+                        Log.e(TAG, "Failed to publish tweet", throwable)
+                    }
+
+                })
+            }
+        }
+    }
+
+    companion object {
+        val TAG = "ComposeActivity"
+    }
+}
